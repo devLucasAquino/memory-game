5ede46cf99b55c5eadfa977c27cd3a3eb2afe20c
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-20 21:09:00 -0300

    feat: checking if card were eguals

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -8,26 +8,44 @@ import saltidao from "../assets/Saltidão.webp";
 import pairulito from "../assets/Pairulito.webp";
 import musculoso from "../assets/Muscleman.webp";
 import { Card } from "./Card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const firstCards = [
+    {img: mordecai},
+    {img: rigby},
+    {img: benson},
+    {img: margaret},
+    {img: eileen},
+    {img: saltidao},
+    {img: fastasmao},
+    {img: musculoso},
+    {img: pairulito},
+];
+
+const secondCards = [...firstCards];
+const cards = firstCards.concat(secondCards);
+
 
 export function GameBoard() {
-    const [ isClick, setIsClick ] = useState<number | null>(null);
-
-    const cards = [
-        {img: mordecai},
-        {img: rigby},
-        {img: benson},
-        {img: margaret},
-        {img: eileen},
-        {img: saltidao},
-        {img: fastasmao},
-        {img: musculoso},
-        {img: pairulito},
-    ];
+    const [ openedCards, setOpenedCards ] = useState<number[]>([]);
+    const [ matchedCards, setMatchedCards ] = useState<number[]>([]);
 
     function openCard(index: number) {
-        setIsClick(index)
+        if (openedCards.length < 2 && !openedCards.includes(index)) {
+          setOpenedCards((prev) => [...prev, index]);
+        }
     }
+
+    useEffect(() => {
+        if (openedCards.length === 2) {
+          const [first, second] = openedCards;
+          if (cards[first].img === cards[second].img) {
+            setMatchedCards((prev) => [...prev, first, second]); 
+          }
+    
+          setTimeout(() => setOpenedCards([]), 1000);
+        }
+      }, [openedCards]);
 
     return (
             <section className="grid grid-cols-6 grid-rows-3 gap-2">
@@ -37,7 +55,7 @@ export function GameBoard() {
                         img={card.img}
                         index={index}
                         onClick={openCard}
-                        isClick={isClick === index}
+                        isClick={openedCards.includes(index) || matchedCards.includes(index)}
                     />
                 ))}
             </section>
